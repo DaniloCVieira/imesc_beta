1503d2826c440fbfd476bcb9d5f4d984ace81b53
DaniloCVieira <vieiradc@yahoo.com.br>
2024-08-20 16:15:00 -0300

    24_08_20

--- inst/www/check_version_desktop.R
+++ inst/www/check_version_desktop.R
@@ -26,3 +26,27 @@ check_version<-function(){
 
 
 }
+
+
+get_git_version<-function(){
+  repo_url <- "https://api.github.com/repos/DaniloCVieira/iMESc/releases/latest"
+  current_version =version
+  # Send a GET request to the GitHub API
+  response <- httr::GET(repo_url)
+
+  # Check if the request was successful
+  if (httr::status_code(response) == 200) {
+
+
+    # Parse the response to a list
+    release_info <- jsonlite::fromJSON(httr::content(response, as = "text"))
+
+    # Extract the tag name (version)
+    latest_version <- release_info$tag_name
+    latest_version<-gsub("iMESc-", "", latest_version)
+
+
+    latest_version
+  }
+
+}
